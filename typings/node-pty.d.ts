2c53bf7ab460d71c85a16cfa8f9396c22b32be58
mrmeku <mrmeku@gmail.com>
2018-12-29 13:35:51 -0700

    Update node-pty.d.ts
    Fixes https://github.com/daviwil/node-pty-prebuilt/issues/10

--- typings/node-pty.d.ts
+++ typings/node-pty.d.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2017, Daniel Imms (MIT License).
  */
 
-declare module 'node-pty' {
+declare module 'node-pty-prebuilt' {
   /**
    * Forks a process as a pseudoterminal.
    * @param file The file to launch.
